31b52682096cc41f4da8380723013b12c3bc9d84
J119082 Katsuma <j119082@g.kogakuin.jp>
2021-10-27 05:16:26 +0000

    Add home, help, about to static_ pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  # => StaticPages#homeと同じ
   get 'static_pages/help'
+  # => StaticPages#help
+  get 'static_pages/about'
+  # GETリクエスト送られたときに反応
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
