33536c7ba4e079460b192495ac1fcae02952bf18
J119082 Katsuma <j119082@g.kogakuin.jp>
2021-12-03 11:47:18 +0000

    Add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # => StaticPages#help
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   # GETリクエスト送られたときに反応
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
