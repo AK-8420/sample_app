3fa520e3db1fdff881f4da9e06fdbe1f5f70c25b
J119082 Katsuma <j119082@g.kogakuin.jp>
2022-01-14 02:22:06 +0000

    Add show action in users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  # GETリクエスト送られたときに反応
+  resources :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root 'application#hello'
