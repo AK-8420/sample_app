e4149945229eb2d68625d577e54d88fe77a23bd1
J119082 Katsuma <j119082@g.kogakuin.jp>
2022-01-14 03:45:13 +0000

    Add error message in sign up view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
-  
+  # GET /users/:id
   def show
     @user = User.find(params[:id])
     #debugger
   end
   
+  # GET /users/new
   def new
+    @user = User.new
   end
+  
+  # POST /users (+ params)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Success (valid params)
+    else
+      # Failure (not valid params)
+      render 'new'
+    end
+  end
+  
+  private
+    # for create action
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
